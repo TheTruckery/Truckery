839f17b49e1c8c70b54a132d464ae01c32d73327
TheTruckery <TheTruckery@gmail.com>
2026-07-19 16:56:10 -0400

    Create App component with initial layout

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: '40px', fontFamily: 'sans-serif', textAlign: 'center' }}>
+      <h1 style={{ color: '#1a2b4c' }}>🚚 Truckery</h1>
+      <p>Foundation is live! Ready to build.</p>
+    </div>
+  )
+}
+
+export default App
